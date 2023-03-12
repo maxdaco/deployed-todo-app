6a3d4131da5eba2fd7d3a72fd467eb7911115942
Massimiliano Da Coreggio <78145752+maxdaco@users.noreply.github.com>
2023-03-12 17:28:35 +0000

    Update db.js

--- server/db.js
+++ server/db.js
@@ -4,10 +4,10 @@ const Pool = require('pg').Pool
 require('dotenv').config()
 
 const pool = new Pool({
-     user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: process.env.DBPORT,
+  user: FV4af1ckPMkODoOG,
+  password: 4rh4ua8xUU0LRvOK,
+  host: todoapp-1zljl-postgresql.todoapp-1zljl.svc.cluster.local,
+  port: 5432,
   database: 'todoapp'
 
 })
